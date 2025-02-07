5e3f5977cd2bb4a48ff7c5c3a055c02a6cd14082
Boluwatife <boluwatife8081@gmail.com>
2025-02-08 00:15:06 +0100

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput
+
+# Upgrade pip and install dependencies using Python 3
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+# Apply migrations
+python3 manage.py migrate
+
+# Collect static files
+python3 manage.py collectstatic --noinput
