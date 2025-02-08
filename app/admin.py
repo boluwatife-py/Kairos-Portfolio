aa6d112083ebc8e9fcf3ee5c00e2d64a7a4bc601
Boluwatife <boluwatife8081@gmail.com>
2025-02-08 07:54:16 +0100

    remove some models from site

--- app/admin.py
+++ app/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 from .models import Heropage, PageTitle, About, WhatOffered, Testimonial, Faq, AudioFile
 
-# Register your models here.
-admin.site.register(PageTitle)
-admin.site.register(Heropage)
-admin.site.register(About)
-admin.site.register(WhatOffered)
+
 admin.site.register(Testimonial)
 admin.site.register(Faq)
 admin.site.register(AudioFile)
